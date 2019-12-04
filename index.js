d775da8e6f904f983be8a2ac9ab85b6438f124ed
Christopher Pitt <cgpitt@gmail.com>
2019-12-04 09:45:47 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const opn = require("opn")
+const { exec } = require("child_process")
+
+exec("composer require laravel-frontend-presets/tailwindcss --dev", (err, stdout, stderr) => {
+    if (err) {
+        console.log("Could not install the preset")
+        return
+    }
+
+    exec("php artisan preset tailwindcss", (err, stdout, stderr) => {
+        if (err) {
+            console.log("Could not use the preset")
+            return
+        }
+
+        exec("npm install && npm run dev", (err, stdout, stderr) => {
+            if (err) {
+                console.log("Could not install NPM dependencies")
+                return
+            }
+
+            exec("composer remove laravel-frontend-presets/tailwindcss", (err, stdout, stderr) => {
+                if (err) {
+                    console.log("Could not uninstall the preset")
+                    return
+                }
+
+                opn("https://tailwindcss.com", {
+                    wait: false,
+                })
+
+                console.log("Done.")
+            })
+        })
+    })
+})
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "printWidth": 120,
+    "tabWidth": 4,
+    "useTabs": false,
+    "semi": false,
+    "singleQuote": false,
+    "quoteProps": "preserve",
+    "jsxSingleQuote": false,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "arrowParens": "avoid",
+    "proseWrap": "never",
+}
